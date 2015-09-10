a7e1e2e496d244b6a3d2d7fab6c1ccd715b13d4c
jamezillar <technomann11@gmail.com>
2015-09-10 18:51:00 +0700

    กำลังแก้ร้านฝั่งลูกค้า

---
+++ pixupfoodtest/view/cus_restaurant_view.php
@@ -0,0 +1,354 @@
+<?php
+include '../api/islogin.php';
+include '../view/navbar.php';
+include '../api/function.php';
+include '../dbconn.php';
+?>
+
+
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+
+
+        <!-- 
+        Boxer Template
+        http://www.templatemo.com/preview/templatemo_446_boxer
+        -->
+        <!-- <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> -->
+
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="keywords" content="">
+        <meta name="description" content="">
+
+        <?php
+        addlink("Test Title");
+        ?>
+        <!-- custom css -->
+        <link rel="stylesheet" href="../assets/css/restaurant_view.css">
+
+    </head>
+    <body>
+
+        <?php cusnavbar(); ?>
+
+
+        <!-- start profile -->
+        <section id="restaurant_view">
+            <div class="profilecontainer">
+                <div class="headprofile">
+                    <img align="left" class="fb-image-lg" src="../assets/images/city-restaurant-lunch-outside.png" alt="Profile image example"/>
+                    <div class="container_status">
+                        <span>Address: <?= $_SESSION["userdata"]["email"] ?></span><span> Tel: <?= $_SESSION["userdata"]["tel"] ?></span>>
+                    </div>
+                    <img align="left" class="fb-image-profile thumbnail" src="http://lorempixel.com/180/180/people/9/" alt="Profile image example"/>
+                    <div class="fb-profile-text">
+                        <h1><?= $_SESSION["userdata"]["firstName"] ?>  <?= $_SESSION["userdata"]["lastName"] ?></h1>
+                        <div class="row lead">
+                            <div id="stars-existing" class="starrr" data-rating='4'></div>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- /container -->
+            <!-- edit profile -->
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="wizard">
+                            <div class="wizard-inner">
+                                <div class="connecting-line"></div>
+                                <ul class="nav nav-tabs" role="tablist">
+
+                                    <li role="presentation" class="active">
+                                        <a href="#step1" data-toggle="tab" aria-controls="step1" role="tab" title="Step 1">
+                                            <span class="round-tab">
+                                                <i class="glyphicon glyphicon-folder-open"></i>
+                                            </span>
+                                        </a>
+                                    </li>
+
+                                    <li role="presentation" class="disabled">
+                                        <a href="#step2" data-toggle="tab" aria-controls="step2" role="tab" title="Step 2">
+                                            <span class="round-tab">
+                                                <i class="glyphicon glyphicon-pencil"></i>
+                                            </span>
+                                        </a>
+                                    </li>
+                                    <li role="presentation" class="disabled">
+                                        <a href="#step3" data-toggle="tab" aria-controls="step3" role="tab" title="Step 3">
+                                            <span class="round-tab">
+                                                <i class="glyphicon glyphicon-picture"></i>
+                                            </span>
+                                        </a>
+                                    </li>
+
+                                    <li role="presentation" class="disabled">
+                                        <a href="#complete" data-toggle="tab" aria-controls="complete" role="tab" title="Complete">
+                                            <span class="round-tab">
+                                                <i class="glyphicon glyphicon-ok"></i>
+                                            </span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <form role="form">
+                                <div class="tab-content">
+                                    <div class="tab-pane active" role="tabpanel" id="step1">
+                                        <h3>Step 1</h3>
+                                        <p>This is step 1</p>
+                                        <ul class="list-inline pull-right">
+                                            <li><button type="button" class="btn btn-primary next-step">Save and continue</button></li>
+                                        </ul>
+                                    </div>
+                                    <div class="tab-pane" role="tabpanel" id="step2">
+                                        <h3>Step 2</h3>
+                                        <p>This is step 2</p>
+                                        <ul class="list-inline pull-right">
+                                            <li><button type="button" class="btn btn-default prev-step">Previous</button></li>
+                                            <li><button type="button" class="btn btn-primary next-step">Save and continue</button></li>
+                                        </ul>
+                                    </div>
+                                    <div class="tab-pane" role="tabpanel" id="step3">
+                                        <h3>Step 3</h3>
+                                        <p>This is step 3</p>
+                                        <ul class="list-inline pull-right">
+                                            <li><button type="button" class="btn btn-default prev-step">Previous</button></li>
+                                            <li><button type="button" class="btn btn-default next-step">Skip</button></li>
+                                            <li><button type="button" class="btn btn-primary btn-info-full next-step">Save and continue</button></li>
+                                        </ul>
+                                    </div>
+                                    <div class="tab-pane" role="tabpanel" id="complete">
+                                        <h3>Complete</h3>
+                                        <p>You have successfully completed all steps.</p>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+
+                    </div>
+                </div>
+            </div>
+        </section> 
+
+
+        <?php
+        show_footer();
+        iconscript();
+        ?>
+
+        <script>
+            (function () {
+                'use strict';
+                $.fn.extend({
+                    filterTable: function () {
+                        return this.each(function () {
+                            $(this).on('keyup', function (e) {
+                                $('.filterTable_no_results').remove();
+                                var $this = $(this),
+                                        search = $this.val().toLowerCase(),
+                                        target = $this.attr('data-filters'),
+                                        $target = $(target),
+                                        $rows = $target.find('tbody tr');
+
+                                if (search == '') {
+                                    $rows.show();
+                                } else {
+                                    $rows.each(function () {
+                                        var $this = $(this);
+                                        $this.text().toLowerCase().indexOf(search) === -1 ? $this.hide() : $this.show();
+                                    })
+                                    if ($target.find('tbody tr:visible').size() === 0) {
+                                        var col_count = $target.find('tr').first().find('td').size();
+                                        var no_results = $('<tr class="filterTable_no_results"><td colspan="' + col_count + '">No results found</td></tr>')
+                                        $target.find('tbody').append(no_results);
+                                    }
+                                }
+                            });
+                        });
+                    }
+                });
+                $('[data-action="filter"]').filterTable();
+            })(jQuery);
+
+            $(function () {
+                // attach table filter plugin to inputs
+                $('[data-action="filter"]').filterTable();
+
+                $('.container').on('click', '.panel-heading span.filter', function (e) {
+                    var $this = $(this),
+                            $panel = $this.parents('.panel');
+
+                    $panel.find('.panel-body').slideToggle();
+                    if ($this.css('display') != 'none') {
+                        $panel.find('.panel-body input').focus();
+                    }
+                });
+                $('[data-toggle="tooltip"]').tooltip();
+            });
+        </script>
+        <script>
+            var __slice = [].slice;
+
+            (function ($, window) {
+                var Starrr;
+
+                Starrr = (function () {
+                    Starrr.prototype.defaults = {
+                        rating: void 0,
+                        numStars: 5,
+                        change: function (e, value) {
+                        }
+                    };
+
+                    function Starrr($el, options) {
+                        var i, _, _ref,
+                                _this = this;
+
+                        this.options = $.extend({}, this.defaults, options);
+                        this.$el = $el;
+                        _ref = this.defaults;
+                        for (i in _ref) {
+                            _ = _ref[i];
+                            if (this.$el.data(i) != null) {
+                                this.options[i] = this.$el.data(i);
+                            }
+                        }
+                        this.createStars();
+                        this.syncRating();
+                        this.$el.on('mouseover.starrr', 'i', function (e) {
+                            return _this.syncRating(_this.$el.find('i').index(e.currentTarget) + 1);
+                        });
+                        this.$el.on('mouseout.starrr', function () {
+                            return _this.syncRating();
+                        });
+                        this.$el.on('click.starrr', 'i', function (e) {
+                            return _this.setRating(_this.$el.find('i').index(e.currentTarget) + 1);
+                        });
+                        this.$el.on('starrr:change', this.options.change);
+                    }
+
+                    Starrr.prototype.createStars = function () {
+                        var _i, _ref, _results;
+
+                        _results = [];
+                        for (_i = 1, _ref = this.options.numStars; 1 <= _ref ? _i <= _ref : _i >= _ref; 1 <= _ref ? _i++ : _i--) {
+                            _results.push(this.$el.append("<i class='fa fa-star-o'></i>"));
+                        }
+                        return _results;
+                    };
+
+                    Starrr.prototype.setRating = function (rating) {
+                        if (this.options.rating === rating) {
+                            rating = void 0;
+                        }
+                        this.options.rating = rating;
+                        this.syncRating();
+                        return this.$el.trigger('starrr:change', rating);
+                    };
+
+                    Starrr.prototype.syncRating = function (rating) {
+                        var i, _i, _j, _ref;
+
+                        rating || (rating = this.options.rating);
+                        if (rating) {
+                            for (i = _i = 0, _ref = rating - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+                                this.$el.find('i').eq(i).removeClass('fa-star-o').addClass('fa-star');
+                            }
+                        }
+                        if (rating && rating < 5) {
+                            for (i = _j = rating; rating <= 4 ? _j <= 4 : _j >= 4; i = rating <= 4 ? ++_j : --_j) {
+                                this.$el.find('i').eq(i).removeClass('fa-star').addClass('fa-star-o');
+                            }
+                        }
+                        if (!rating) {
+                            return this.$el.find('i').removeClass('fa-star').addClass('fa-star-o');
+                        }
+                    };
+
+                    return Starrr;
+
+                })();
+                return $.fn.extend({
+                    starrr: function () {
+                        var args, option;
+
+                        option = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+                        return this.each(function () {
+                            var data;
+
+                            data = $(this).data('star-rating');
+                            if (!data) {
+                                $(this).data('star-rating', (data = new Starrr($(this), option)));
+                            }
+                            if (typeof option === 'string') {
+                                return data[option].apply(data, args);
+                            }
+                        });
+                    }
+                });
+            })(window.jQuery, window);
+
+            $(function () {
+                return $(".starrr").starrr();
+            });
+
+            $(document).ready(function () {
+
+                $('#stars').on('starrr:change', function (e, value) {
+                    $('#count').html(value);
+                });
+
+                $('#stars-existing').on('starrr:change', function (e, value) {
+                    $('#count-existing').html(value);
+                });
+            });
+        </script>
+        <script>
+            $(document).ready(function () {
+                //Initialize tooltips
+                $('.nav-tabs > li a[title]').tooltip();
+
+                //Wizard
+                $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+
+                    var $target = $(e.target);
+
+                    if ($target.parent().hasClass('disabled')) {
+                        return false;
+                    }
+                });
+
+                $(".next-step").click(function (e) {
+
+                    var $active = $('.wizard .nav-tabs li.active');
+                    $active.next().removeClass('disabled');
+                    nextTab($active);
+
+                });
+                $(".prev-step").click(function (e) {
+
+                    var $active = $('.wizard .nav-tabs li.active');
+                    prevTab($active);
+
+                });
+            });
+
+            function nextTab(elem) {
+                $(elem).next().find('a[data-toggle="tab"]').click();
+            }
+            function prevTab(elem) {
+                $(elem).prev().find('a[data-toggle="tab"]').click();
+            }
+        </script>
+
+
+    </body>
+</html>
