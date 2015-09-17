4cc2d2ddef9b54b5bb0da8914e7c4145c27010f2
thanareee <peepolike@gmail.com>
2015-09-18 05:41:45 +0700

    แก้ navbar res

--- pixupfoodtest/template/restaurant-navbar.php
+++ pixupfoodtest/template/restaurant-navbar.php
@@ -15,7 +15,7 @@ if (isset($_SESSION["islogin"])) {
     <!-- end preloader -->
     <!-- start navigation -->
     <nav class="navbar navbar-default navbar-fixed-top templatemo-nav" role="navigation">
-        <div class="container" style="margin-left:100px;margin-right:40px;height:70px;width:auto;">
+        <div class="container-fluid" >
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
@@ -23,31 +23,33 @@ if (isset($_SESSION["islogin"])) {
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a href="#" class="navbar-brand">Pixup</a>
+                <a href="#" class="navbar-brand" style="margin-left:100px;">Pixup</a>
                 <a href="#" class="navbar-brand" style="color:rgba(0,0,32,1);padding-left: 0px;">Food</a>
             </div>
-            <ul class="nav navbar-nav navbar-right" id="navright-after" >
-                <li>
-                    <a href="../view/res_restaurant_manage_order.php"  role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-align-left"></span></a> 
-                </li>
-                <li>
-                    <a href="../view/res_restaurant_manage_today.php"  role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-comment"></span></a> 
-                </li>
-                <li>
-                    <a href="../view/res_restaurant_manage_menulist.php"  role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-list-alt"></span></a> 
-                </li>
-                <li>
-                    <a href="../view/res_restaurant_manage_calendar.php"  role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-calendar"></span></a> 
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"><?= $data["name"] . " " . $id ?></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="../view/res_restaurant_manage_edit.php"><span class="glyphicon glyphicon-cog"></span>&nbsp;&nbsp;แก้ไขข้อมูลร้าน</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="../api/logout.php"><span class="glyphicon glyphicon-log-out"></span>&nbsp;&nbsp;ออกจากระบบ</a></li>
-                    </ul>
-                </li>
-            </ul>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"> 
+                <ul class="nav navbar-nav navbar-right" id="navright-after" >
+                    <li>
+                        <a href="../view/res_restaurant_manage_order.php"  role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-align-left"></span></a> 
+                    </li>
+                    <li>
+                        <a href="../view/res_restaurant_manage_today.php"  role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-comment"></span></a> 
+                    </li>
+                    <li>
+                        <a href="../view/res_restaurant_manage_menulist.php"  role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-list-alt"></span></a> 
+                    </li>
+                    <li>
+                        <a href="../view/res_restaurant_manage_calendar.php"  role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-calendar"></span></a> 
+                    </li>
+                    <li class="dropdown" >
+                        <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true" style="margin-left:10px; margin-right:20px"><?= $data["name"] . " " . $id ?></a>
+                        <ul class="dropdown-menu" style=" margin-right:20px">
+                            <li><a href="../view/res_restaurant_manage_edit.php"><span class="glyphicon glyphicon-cog"></span>&nbsp;&nbsp;แก้ไขข้อมูลร้าน</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="../api/logout.php"><span class="glyphicon glyphicon-log-out"></span>&nbsp;&nbsp;ออกจากระบบ</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
     </nav>
