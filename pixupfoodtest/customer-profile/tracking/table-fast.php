3cf53ce4628b55a49fc99bc736c318633a5ca860
thanareee <peepolike@gmail.com>
2015-10-14 09:38:59 +0700

    โชว์ฟาส ในตาราง

--- pixupfoodtest/customer-profile/tracking/table-fast.php
+++ pixupfoodtest/customer-profile/tracking/table-fast.php
@@ -1,31 +1,53 @@
 <?php
-
 session_start();
 include '../../dbconn.php';
 $cusid = $_SESSION["userdata"]["id"];
-$orderRes = $con->query("SELECT * FROM `normal_order` WHERE customer_id = '$cusid'");
+$orderRes = $con->query("SELECT fast_order.id as fast_id, order_status.description,"
+        . " fast_order.quantity as qty, restaurant.name, fast_order.main_menu_id "
+        . "FROM `fast_order` "
+        . "LEFT JOIN order_status ON order_status.id = fast_order.status "
+        . "LEFT JOIN restaurant ON restaurant.id = fast_order.restaurant_id "
+        . "WHERE fast_order.customer_id = '$cusid' "
+        . "and fast_order.status != '7' "
+        . "GROUP by fast_order.id");
 if ($orderRes->num_rows == 0) {
     ?>
     <tr>
     <tr><td colspan="10" class="warning" style="text-align: center;"><h4>ยังไม่มีรายการสั่งซื้อ</h4></td></tr>                  
     </tr>
     <?php
-
 } else {
     $i = 1;
     while ($orderData = $orderRes->fetch_assoc()) {
         ?>
         <tr>
-            <td><?= $i++;?></td>
-            <td>102458</td>                         
-            <td>ข้าวกล้อง+ผัดกระเพาหมู+ไข่ดาว</td>
-            <td>50</td>
-            <td>ปรุงอาหาร</td>
+            <td><?= $i++; ?></td>
+            <td><?= $orderData["fast_id"] ?></td>                         
+            <td>
+                <?php
+                $menuid = $orderData["main_menu_id"];
+                $menuid = "(" . $menuid . ")";
+                $name = "";
+                $resName = $con->query("SELECT  main_menu.name FROM main_menu WHERE main_menu.id IN $menuid");
+                $count = 0;
+                while ($food = $resName->fetch_assoc()) {
+
+                    $name = $food["name"];
+                    // $menustr .= $name;
+                    $count++;
+                    if ($count < $resName->num_rows) {
+                        $name.="+";
+                    }
+                    echo $name;
+                }
+                ?>
+            </td>
+            <td><?= $orderData["qty"] ?></td>
+            <td><?= $orderData["description"] ?></td>
             <td class="text-center"><button class="btn btn-info btn-xs" data-toggle="modal" data-target='#track' href="#track"><span class="glyphicon glyphicon-eye-open"></span> แสดง</button></td>
             <td class="text-center"><button class="btn btn-warning btn-xs" data-toggle="modal" data-target='#transf' href="#transf" disabled="disabled"><span class="glyphicon glyphicon-eye-open"></span> อัพโหลด</button></td>
         </tr>
         <?php
-
     }
 }
 ?>
--- pixupfoodtest/restaurant/ajax_fetchdataFastOrder.php
+++ pixupfoodtest/restaurant/ajax_fetchdataFastOrder.php
@@ -2,7 +2,19 @@
 include '../dbconn.php';
 $resid = @$_POST["resid"];
 
-$fastOrderRes = $con->query("SELECT * FROM `normal_order` where restaurant_id = '$resid'");
+$fastOrderRes = $con->query("SELECT fast_order.id as fast_id, fast_order.delivery_date, "
+        . "fast_order.delivery_time, order_status.description,fast_order.shippingAddress_id, "
+        . "fast_order.customer_id , quantity as qty , customer.firstName, customer.lastName , "
+        . "fast_order.main_menu_id "
+        . "FROM `fast_order` "
+        . "LEFT JOIN order_status ON order_status.id = fast_order.status"
+        . " LEFT JOIN restaurant ON restaurant.id = fast_order.restaurant_id "
+        . "LEFT JOIN request_fast_order ON request_fast_order.fast_id = fast_order.id "
+        . "LEFT JOIN customer ON customer.id = fast_order.customer_id "
+        . "WHERE request_fast_order.restaurant_id = '$resid' "
+        . "AND request_fast_order.priority = '1' "
+        . "and fast_order.status != '7' "
+        . "ORDER BY fast_order.order_time DESC");
 $i = 1;
 if ($fastOrderRes->num_rows == 0) {
     ?>
@@ -10,66 +22,52 @@ if ($fastOrderRes->num_rows == 0) {
     <?php
 } else {
     while ($fastOrderData = $fastOrderRes->fetch_assoc()) {
-
-        /* if(){
-          ถ้าเพิ่งมา count > 0 ให้ tr เป็น class danger'
-          <tr class="danger">
-          <td style="text-align: center">$i++;</td>
-          <td><?= $fastOrderData["id"] ?></td>
-          <td><?= $$fastOrderData["cusname"] ?></td>
-          <td><?= $fastOrderData["foodsetname"] ?></td>
-          <td style="text-align: center"><?= $fastOrderData["amountbox"] ?></td>
-          <td><?= $fastOrderData["delivery_date"] ?>&nbsp;<?= $fastOrderData["delivery_time"] ?> </td>
-          <td style="text-align: center">----- </td>
-          <td class="text-center">
-          <button class="btn btn-info btn-xs fastOrderView" id="fastOrderView<?= $fastOrderData["id"] ?>" >
-          <span class="glyphicon glyphicon-eye-open"></span>
-          แสดง
-          </button>
-          </td>
-          <td class="text-center">
-          <button class="btn btn-success btn-xs acceptFastOrder" id="acceptFastOrder<?= $fastOrderData["id"] ?>">
-          <span class="glyphicon glyphicon-check"></span>
-          รับ
-          </button>
-          </td>
-          <td class="text-center">
-          <button class="btn btn-danger btn-xs ignoreFastOrder" id="ignoreFastOrder<?= $fastOrderData["id"] ?>">
-          <span class="glyphicon glyphicon-trash"></span>
-          ปฏิเสธ
-          </button>
-          </td>
-          </tr>
-
-
-          } */
         ?>
         <tr >
-            <td style="text-align: center"><?=$i++;?></td>
-            <td><?= $fastOrderData["id"] ?></td>                         
-            <td><?= $fastOrderData["id"] ?></td>
-            <td><?= $fastOrderData["id"] ?></td>
-            <td style="text-align: center"><?= $fastOrderData["id"] ?></td>
+            <td style="text-align: center"><?= $i++; ?></td>
+            <td><?= $fastOrderData["fast_id"] ?></td>                         
+            <td><?= $fastOrderData["firstName"] . '&nbsp;' . $fastOrderData["lastName"] ?></td>
+            <td>
+                <?php
+                $menuid = $fastOrderData["main_menu_id"];
+                $menuid = "(" . $menuid . ")";
+                $name = "";
+                
+                $resName = $con->query("SELECT  main_menu.name FROM main_menu WHERE main_menu.id IN $menuid");
+                $count = 0;
+                while ($food = $resName->fetch_assoc()) {
+
+                    $name = $food["name"];
+                    $count++;
+                    if ($count < $resName->num_rows) {
+                        $name.="+";
+                    }
+                    echo $name;
+                }
+                ?>
+            </td>
+            <td style="text-align: center"><?= $fastOrderData["qty"] ?></td>
             <td><?= $fastOrderData["delivery_date"] ?>&nbsp;<?= $fastOrderData["delivery_time"] ?> </td>
             <td style="text-align: center">----- </td>
             <td class="text-center">
-                <button class="btn btn-info btn-xs fastOrderView" id="fastOrderView<?= $fastOrderData["id"] ?>" >
+                <button class="btn btn-info btn-xs fastOrderView" data-id="<?= $fastOrderData["fast_id"] ?>" >
                     <span class="glyphicon glyphicon-eye-open"></span> 
                     แสดง
                 </button>
             </td>
             <td class="text-center">
-                <button class="btn btn-success btn-xs acceptFastOrder" id="acceptFastOrder<?= $fastOrderData["id"] ?>">
+                <button class="btn btn-success btn-xs acceptFastOrder" data-id="<?= $fastOrderData["fast_id"] ?>">
                     <span class="glyphicon glyphicon-check"></span> 
                     รับ
                 </button>
             </td>
             <td class="text-center">
-                <button class="btn btn-danger btn-xs ignoreFastOrder" id="ignoreFastOrder<?= $fastOrderData["id"] ?>">
+                <button class="btn btn-danger btn-xs ignoreFastOrder" data-id="<?= $fastOrderData["fast_id"] ?>">
                     <span class="glyphicon glyphicon-trash"></span> 
                     ปฏิเสธ
                 </button>
             </td>
         </tr>
     <?php }
-} ?>
+}
+?>
